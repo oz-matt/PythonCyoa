8b3a13d603d6bf3ff99ab95ad9ebae15d6d3253f
Matthew Morra <matt.morra@imcorp.com>
2017-12-07 21:15:42 -0500

    First 3 nodes created with kinda modular implementation

--- Amacraftaah.py
+++ Amacraftaah.py
@@ -9,10 +9,10 @@ from TitleForm import *
 class TestApp(npyscreen.NPSAppManaged):
     def onStart(self):
         npyscreen.setTheme(npyscreen.Themes.ColorfulTheme)
-
         self.registerForm('MAIN', TitleForm())
-        self.registerForm('TFRM2', StoryForm(split_str_into_list(T1D1str),['ch1','ch2']))
-        self.registerForm('TFRM1', StoryForm(split_str_into_list(T1D1str),['ch1','ch5']))
+        self.registerForm('T1N1', StoryForm(split_str_into_list(T1N1str),['Drink water','Refuse water','Jump out of the vehicle'], ['T1N2', 'T1N2', 'T1N3']))
+        self.registerForm('T1N2', StoryForm(split_str_into_list(T1N2str),['Continue asking questions','Stay silent'], ['T1N3', 'T1N3']))
+        self.registerForm('T1N3', StoryForm(split_str_into_list(T1N3str),['The end :['], ['T1N3']))
 
 if __name__ == "__main__":
     App = TestApp()
